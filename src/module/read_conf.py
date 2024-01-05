4a6118d295f2946e00f73592c6b86a38e19f116e
henntaidesu <58099835+henntaidesu@users.noreply.github.com>
2024-01-05 23:38:30 +0800

    update

--- src/module/read_conf.py
+++ src/module/read_conf.py
@@ -35,3 +35,56 @@ class read_conf:
         papers_need = int(self.config.get('cnki paper passkey', 'papers_need'))
         return web_zoom, keyword, papers_need
 
+    def cnki_skip_db(self):
+        newpaper = self.config.get('cnki skip jump', '报纸')
+        journal = self.config.get('cnki skip jump', '期刊')
+        journal_A = self.config.get('cnki skip jump', '特色期刊')
+        master = self.config.get('cnki skip jump', '硕士')
+        PhD = self.config.get('cnki skip jump', '博士')
+
+        if newpaper == "True":
+            newpaper = True
+        else:
+            newpaper = False
+
+        if journal == "True":
+            journal = True
+        else:
+            journal = False
+
+        if journal_A == "True":
+            journal_A = True
+        else:
+            journal_A = False
+
+        if master == "True":
+            master = True
+        else:
+            master = False
+
+        if PhD == "True":
+            PhD = True
+        else:
+            PhD = False
+
+        return [newpaper, journal, master, PhD]
+
+
+class CNKI:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('conf.ini', encoding='utf-8')
+
+    def read_cnki_date(self):
+        year = int(self.config.get('cnki date', 'year'))
+        moon = int(self.config.get('cnki date', 'moon'))
+        day = int(self.config.get('cnki date', 'day'))
+        return year, moon, day
+
+    def write_cnki_date(self, year, moon, day):
+        self.config.set('cnki date', 'year', year)
+        self.config.set('cnki date', 'moon', moon)
+        self.config.set('cnki date', 'day', day)
+        with open('conf.ini', 'w', encoding='utf-8') as configfile:
+            self.config.write(configfile)
+
